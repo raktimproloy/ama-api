8d64341226992b3480a6d3311386a8ea620b70bf
raktimproloy <raktimproloy@gmail.com>
2023-09-17 02:51:33 +0600

    update status code

--- src/routes/events/event.js
+++ src/routes/events/event.js
@@ -93,9 +93,7 @@ router.get("/view/all",  async(req, res) => {
     try{
         const events = Event.find()
             .then((data) => {
-                res.status(200).json({
-                    resutl: data
-                })
+                res.status(200).send(data)
             })
             .catch(err => {
                 res.status(500).json({
